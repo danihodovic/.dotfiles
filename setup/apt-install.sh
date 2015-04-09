0b5343676df348252af1f610da53327af537bbb4
Dani Hodovic <danihgit@gmail.com>
2015-04-09 16:07:51 +0200

    .

--- setup/apt-install.sh
+++ setup/apt-install.sh
@@ -1,19 +1,15 @@
-apt-get install git -y
-apt-get install vim -y
-apt-get install xbindkeys -y
-apt-get install dmenu -y
-apt-get install chromium-browser -y
-apt-get install cmake -y
-apt-get install python-dev -y
+sudo apt-get install git -y
+sudo apt-get install vim -y
+sudo apt-get install xbindkeys -y
+sudo apt-get install dmenu -y
+sudo apt-get install chromium-browser -y
+sudo apt-get install cmake -y
+sudo apt-get install python-dev -y
+sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-get update
+sudo apt-get install oracle-java8-installer --yes
 
-add-apt-repository ppa:webupd8team/java -y
-add-apt-repository ppa:neovim-ppa/unstable -y
-apt-get update
-apt-get install oracle-java8-installer --yes
-apt-get install neovim -y
-
-apt-get install python -y
-wget -O - https://bootstrap.pypa.io/get-pip.py | python
-pip install neovim
+sudo apt-get install python -y
+sudo wget -O - https://bootstrap.pypa.io/get-pip.py | python
 
 # apt-get update && apt-get dist-upgrade
---
+++ setup/vim-daily-apt.sh
@@ -0,0 +1,11 @@
+sudo apt-get remove vim vim-runtime gvim vim-tiny vim-common vim-gui-common -y
+sudo apt-add-repository ppa:pkg-vim/vim-daily -y
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
+sudo apt-get update
+sudo apt-get install vim vim-gtk -y
+sudo apt-get install vim vim-gtk -y
+sudo apt-get install python -y
+sudo apt-get install neovim -y
+
+sudo wget -O - https://bootstrap.pypa.io/get-pip.py | python
+sudo pip install neovim
