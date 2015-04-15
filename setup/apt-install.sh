70f50c4ec4c696b1b2a198d5ecfe37d016d770c8
Dani Hodovic <danihgit@gmail.com>
2015-04-15 20:00:03 +0200

    .

--- setup/apt-install.sh
+++ setup/apt-install.sh
@@ -1,15 +1,18 @@
 sudo apt-get install git -y
-sudo apt-get install vim -y
 sudo apt-get install xbindkeys -y
-sudo apt-get install dmenu -y
 sudo apt-get install chromium-browser -y
 sudo apt-get install cmake -y
 sudo apt-get install python-dev -y
+
 sudo add-apt-repository ppa:webupd8team/java -y
 sudo apt-get update
 sudo apt-get install oracle-java8-installer --yes
 
 sudo apt-get install python -y
 sudo wget -O - https://bootstrap.pypa.io/get-pip.py | python
+
+sudo add-apt-repository ppa:chilicuil/sucklesstools -y
+sudo apt-get update
+sudo apt-get install suckless-tools
 
 # apt-get update && apt-get dist-upgrade
