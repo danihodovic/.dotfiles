6f4a989144189ffed507d172b8d9b364dbf8742d
Dani Hodovic <danihgit@gmail.com>
2015-04-26 01:08:17 +0200

    -

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,34 +1,38 @@
 #!/usr/bin/env bash
-DIR=~/.bash
+DIR=`dirname $0`
 RCFILES=$DIR/rcfiles
-bash "`dirname $0`/git-setup.sh"
+bash "$DIR/git-setup.sh"
 
 echo "Setting up symlinks"
 
 if [ ! -L ~/.vimrc ]; then
-  ln -s $DIR/vimrc ~/.vimrc
+    ln -s $DIR/vimrc ~/.vimrc
 fi
 
 if [ ! -L ~/.nvimrc ]; then
-  ln -s $DIR/vimrc ~/.nvimrc
+    ln -s $DIR/vimrc ~/.nvimrc
 fi
 
 if [ ! -L ~/.nvim ]; then
-  ln -s ~/.vim ~/.nvim
+    ln -s ~/.vim ~/.nvim
 fi
 
 if [ ! -L ~/.inputrc ]; then 
-  ln -s $RCFILES/inputrc ~/.inputrc
+    ln -s $RCFILES/inputrc ~/.inputrc
+fi
+
+if [ ! -L ~/.tmux.conf ]; then 
+    ln -s "`dirname $DIR`/conf/tmux.conf" ~/.tmux.conf
 fi
 
 if [ ! -L ~/.xbindkeysrc ]; then
-  ln -s $RCFILES/xbindkeysrc ~/.xbindkeysrc
+    ln -s $RCFILES/xbindkeysrc ~/.xbindkeysrc
 fi
 
 if [ ! -L ~/.sqliterc ]; then 
-  ln -s $RCFILES/sqliterc ~/.sqliterc
+    ln -s $RCFILES/sqliterc ~/.sqliterc
 fi
 
 if [ ! -L ~/.jshintrc ]; then 
-  ln -s $RCFILES/jshintrc ~/.jshintrc
+    ln -s $RCFILES/jshintrc ~/.jshintrc
 fi
