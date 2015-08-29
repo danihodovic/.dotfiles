24215af698b288e8dd4f1d2172ae8f4809edd4d9
Dani Hodovic <danihgit@gmail.com>
2015-08-29 20:04:11 +0200

    Moved non apt-packages to a separate script

---
+++ install/alt-packages.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+read -p "Install python+pip?"       INSTALL_PYTHON
+read -p "Install Dropbox?"          INSTALL_DROPBOX
+read -p "Install bro?"              INSTALL_BRO
+read -p "Install howdoi?"           INSTALL_HOWDOI
+read -p "Install commandlinefu?"    INSTALL_CLF
+
+case $INSTALL_PYTHON in
+    y|Y)
+        sudo apt-get install python -y
+        sudo wget -O - https://bootstrap.pypa.io/get-pip.py | python
+        ;;
+esac
+
+case $INSTALL_DROPBOX in
+    y|Y)
+        sudo apt-get install python-gtk2
+        wget -O tempfile https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.02.12_amd64.deb
+        sudo dpkg -i tempfile
+        dropbox start -i
+        rm tempfile ;;
+esac
+
+case $INSTALL_BRO in
+    y|Y)
+        sudo apt-get install ruby ruby-dev -y
+        sudo gem install bropages
+        ;;
+esac
+
+case $INSTALL_HOWDOI in
+    y|Y)
+        sudo apt-get install libxml2-dev libxslt1-dev -y
+        sudo pip install howdoi
+        ;;
+esac
+
+case $INSTALL_CLF in
+    y|Y)
+        sudo pip install clf
+        ;;
+esac
--- install/apt-packages.sh
+++ install/apt-packages.sh
@@ -1,54 +1,23 @@
-read -p "Install Dropbox?"          INSTALL_DROPBOX
-read -p "Install Chromium?"         INSTALL_CHROME
 read -p "Install Oracle java?"      INSTALL_JAVA
-read -p "Install python+pip?"       INSTALL_PYTHON
 read -p "Install suckless-tools?"   INSTALL_SUCKLESS
-read -p "Install bro?"              INSTALL_BRO
-read -p "Install howdoi?"           INSTALL_HOWDOI
-read -p "Install commandlinefu?"    INSTALL_CLF
 read -p "Install numix?"            INSTALL_NUMIX
 
 sudo apt-get update
-sudo apt-get install xbindkeys -y
-sudo apt-get install cmake -y
-sudo apt-get install python-dev -y
 sudo apt-get install sshfs -y
 sudo apt-get install tmux -y
 sudo apt-get install curl -y
+sudo apt-get install cmake -y
 sudo apt-get install git-extras -y
+sudo apt-get install python-dev -y
 
 # Move to next monitor scripts
 sudo apt-get install xdotool wmctrl -y
-
-
-case $INSTALL_DROPBOX in
-    y|Y)
-        sudo apt-get install python-gtk2
-        wget -O tempfile https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.02.12_amd64.deb
-        sudo dpkg -i tempfile
-        dropbox start -i
-        rm tempfile ;;
-esac
-
-
-case $INSTALL_CHROME in
-    y|Y) sudo apt-get install chromium-browser -y ;;
-esac
-
 
 case $INSTALL_JAVA in
     y|Y)
         sudo add-apt-repository ppa:webupd8team/java -y
         sudo apt-get update
         sudo apt-get install oracle-java8-installer --yes
-        ;;
-esac
-
-
-case $INSTALL_PYTHON in
-    y|Y)
-        sudo apt-get install python -y
-        sudo wget -O - https://bootstrap.pypa.io/get-pip.py | python
         ;;
 esac
 
@@ -71,24 +40,4 @@ case $INSTALL_SUCKLESS in
 esac
 
 
-case $INSTALL_BRO in
-    y|Y)
-        sudo apt-get install ruby ruby-dev -y
-        sudo gem install bropages
-        ;;
-esac
 
-
-case $INSTALL_CLF in
-    y|Y)
-        sudo pip install clf
-        ;;
-esac
-
-
-case $INSTALL_HOWDOI in
-    y|Y)
-        sudo apt-get install libxml2-dev libxslt1-dev
-        sudo pip install howdoi
-        ;;
-esac
