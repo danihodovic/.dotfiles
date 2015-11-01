f631ac593d0be96d5171fba4436fe3234f71e3d2
Dani Hodovic <danihgit@gmail.com>
2015-11-01 23:42:13 +0100

    Added nvm & node install

--- install/third-party.sh
+++ install/third-party.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
 
-read -p "Install Dropbox?"                      INSTALL_DROPBOX
+read -p "Install nvm?"              INSTALL_NVM
+read -p "Install Dropbox?"          INSTALL_DROPBOX
 read -p "Install Oracle java?"      INSTALL_JAVA
 read -p "Install numix?"            INSTALL_NUMIX
 read -p "Install suckless-tools?"   INSTALL_SUCKLESS
 
+case $INSTALL_NVM in
+    y)
+        git clone https://github.com/creationix/nvm.git ~/.nvm
+        read -p "Install latest node and set as stable?" INSTALL_NODE
+        case $INSTALL_NODE in
+            y)
+                source ~/.nvm/nvm.sh
+                nvm install node
+                nvm alias default node
+        esac
+esac
 
 case $INSTALL_DROPBOX in
-    y|Y)
+    y)
         sudo apt-get install python-gtk2
         wget -O tempfile https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.02.12_amd64.deb
         sudo dpkg -i tempfile
@@ -17,7 +29,7 @@ esac
 
 
 case $INSTALL_JAVA in
-    y|Y)
+    y)
         sudo add-apt-repository ppa:webupd8team/java -y
         sudo apt-get update
         sudo apt-get install oracle-java8-installer --yes
@@ -26,7 +38,7 @@ esac
 
 
 case $INSTALL_NUMIX in
-    y|Y)
+    y)
         sudo add-apt-repository ppa:numix/ppa -y
         sudo apt-get update
         sudo apt-get install numix-gtk-theme numix-icon-theme-circle -y
@@ -35,7 +47,7 @@ esac
 
 
 case $INSTALL_SUCKLESS in
-    y|Y)
+    y)
         sudo add-apt-repository ppa:minos-archive/main -y
         sudo apt-get update
         sudo apt-get install suckless-tools
