ed61ed41fdbaee4e622bc2c7f4a08ec99c6136e9
Dani Hodovic <danihgit@gmail.com>
2015-11-16 19:02:27 +0100

    Added jscs and jshint as installation options

--- install/npm-packages.sh
+++ install/npm-packages.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
 
+read -p "Install jscs?"                                 PARAM_JSCS
+read -p "Install jshint?"                               PARAM_JSHINT
+
 read -p "Install Typescript? [y/n]"                     PARAM_TYPESCRIPT
 read -p "Install clausreinke/typescript-tools? [y/n]"   PARAM_TS_TOOLS
 read -p "Install vvakame/typescript-formatter? [y/n]"   PARAM_TS_FORMATTER
+
+
+case $PARAM_JSCS in
+    y|Y )
+        npm install -g jscs;;
+esac
+
+case $PARAM_JSHINT in
+    y|Y )
+        npm install -g jshint;;
+esac
 
 case $PARAM_TYPESCRIPT in
     y|Y )
