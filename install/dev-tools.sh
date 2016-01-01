83c2572c58c9184b556663d98c97a8d029d96aa9
Dani Hodovic <danihgit@gmail.com>
2016-01-01 14:47:22 +0100

    Separated installations with plugins and setups

--- install/dev-tools.sh
+++ install/dev-tools.sh
@@ -9,10 +9,19 @@ set -u
 
 read -p "Install gnome terminal from the latest ppa? " -n 1 -r  install_gnome_terminal
 echo
-read -p "Install zsh (+antigen) and tmux (+tpm) ? " -n 1 -r  install_zsh
+read -p "Install zsh and tmux? " -n 1 -r  install_zsh
 echo
 read -p "Install Neovim ppa + neovim pip? " -n 1 -r         install_neovim
 echo
+read -p "Install zsh plugin manager antigen? " -n 1 -r  install_antigen
+echo
+read -p "Install nvim plugin manager vim-plug? " -n 1 -r  install_vim_plug
+echo
+read -p "Install tmux plugin manager tpm? " -n 1 -r  install_tpm
+echo
+read -p "Setup nvim?" -n 1 -r  setup_nvim
+echo
+
 
 case $install_gnome_terminal in
   y)
@@ -36,45 +45,12 @@ case $install_zsh in
         echo "Installing tmux"
         sudo apt-get install -y tmux
 
-        if [ ! -d ~/.tmux/plugins/tpm ]; then
-            echo "Installing tpm"
-            git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-        fi
 
-        if [ ! -f "$ANTIGEN_PATH" ]; then
-            echo "Installing antigen"
-            ANTIGEN_URL=https://raw.githubusercontent.com/zsh-users/antigen/master/antigen.zsh
-            curl -fLo $ANTIGEN_PATH --create-dirs $ANTIGEN_URL
-        fi
 esac
+
 
 case $install_neovim in
     y|Y)
-
-        # Check so that the env var is set. This probably means our dotfiles are
-        # installed. -z checks that the length of the env var is > 0
-        # Todo check if this dir is not a symlink
-        if [ -z "$NVIM_DIR" ]; then
-            echo "[Error] Setup your dotfiles and source zshrc before attempting to install.."
-            exit 1
-        fi
-
-        if [ ! -d "$NVIM_DIR" ]; then
-            echo "...Setting up nvim directories and symlinks..."
-            mkdir -p ~/.config/nvim
-            # We can assume .dotfiles exist because of $NVIM_DIR being set
-            ln -s ~/.dotfiles/vimrc ~/.config/nvim/init.vim
-        else
-	        echo "Neovim config files already set up"
-        fi
-
-        if [ ! -f "$NVIM_DIR/autoload/plug.vim" ]; then
-            echo "...Installing vim-plug..."
-            PLUG_URL=https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-            curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs $PLUG_URL
-        else
-            echo "...vim-plug already installed..."
-        fi
 
         # Neovim from pip needs to be installed for the python plugins to work
         set +e
@@ -106,3 +82,76 @@ case $install_neovim in
         ;;
 esac
 
+
+case $install_antigen in
+  y)
+      if [ -z "$ANTIGEN_PATH" ]; then
+          echo "[Error] Setup your dotfiles and source zshrc before attempting to install.."
+          exit 1
+      fi
+
+      if [ ! -f "$ANTIGEN_PATH" ]; then
+          echo "Installing antigen"
+          ANTIGEN_URL=https://raw.githubusercontent.com/zsh-users/antigen/master/antigen.zsh
+          curl -fLo $ANTIGEN_PATH --create-dirs $ANTIGEN_URL
+      fi
+esac
+
+
+case $install_vim_plug in
+  y)
+      # Check so that the env var is set. This probably means our dotfiles are
+      # installed. -z checks that the length of the env var is > 0
+      # Todo check if this dir is not a symlink
+      if [ -z "$NVIM_DIR" ]; then
+          echo "[Error] Setup your dotfiles and source zshrc before attempting to install.."
+          exit 1
+      fi
+
+      if [ ! -d "$NVIM_DIR" ]; then
+          echo "...Creating ~/.config/nvim..."
+          mkdir -p ~/.config/nvim
+      else
+        echo "Neovim config files already set up"
+      fi
+
+      if [ ! -f "$NVIM_DIR/autoload/plug.vim" ]; then
+          echo "...Installing vim-plug..."
+          PLUG_URL=https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+          curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs $PLUG_URL
+      else
+          echo "vim-plug already installed..."
+      fi
+esac
+
+
+case $install_tpm in
+  y)
+      if [ ! -d ~/.tmux/plugins/tpm ]; then
+          echo "Installing tpm"
+          git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+      else
+         echo "Tpm already installed"
+      fi
+esac
+
+
+case $setup_nvim in
+  y)
+      if [ -z "$NVIM_DIR" ]; then
+          echo "[Error] Setup your dotfiles and source zshrc before attempting to install.."
+          exit 1
+      fi
+
+      if [ ! -d "$NVIM_DIR" ]; then
+          mkdir -p ~/.config/nvim
+      fi
+
+      if [ ! -L "${NVIM_DIR}/init.vim" ]; then
+          echo "Setting up nvim config..."
+          ln -s ~/.dotfiles/vimrc ~/.config/nvim/init.vim
+      else
+          echo "Neovim config files already set up"
+      fi
+
+esac
