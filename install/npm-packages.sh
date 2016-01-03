b6c85439b2d551e2057f51be0629f1a3e0057b35
Dani Hodovic <danihgit@gmail.com>
2016-01-03 03:27:19 +0100

    Added tldr to installs

--- install/npm-packages.sh
+++ install/npm-packages.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
 set -e
 
+read -p "Install tldr-pages? "                   -n 1 -r install_tldr
+echo
 read -p "Install jscs? "                         -n 1 -r   param_jscs
 echo
 read -p "Install jshint? "                       -n 1 -r   param_jshint
 echo
 read -p "Install fixmyjs? "                      -n 1 -r   param_fixmyjs
 echo
-
 read -p "Install Typescript? "                   -n 1 -r   param_typescript
 echo
 read -p "Install clausreinke/typescript-tools? " -n 1 -r   param_ts_tools
 echo
 read -p "Install vvakame/typescript-formatter? " -n 1 -r   param_ts_formatter
 echo
+echo "..."
+
+case $install_tldr in
+  y) npm install -g tldr
+esac
 
 case $param_jscs in
     y|Y )
