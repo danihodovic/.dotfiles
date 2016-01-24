18e72c774f20d6cc8c4cd3d63f60a2184fcd3652
Dani Hodovic <dani.hodovic@gmail.com>
2016-01-24 01:19:37 +0100

    Rewrote setup.py for testability and added unit tests

--- setup/setup.py
+++ setup/setup.py
@@ -5,69 +5,60 @@ Script to setup all of the symlinks
 import os
 import argparse
 
-HOME_DIR     = os.path.expandvars('${HOME}')
-DOTFILES_DIR = HOME_DIR + '/.dotfiles'
-CONF_DIR     = HOME_DIR + '/.dotfiles/conf'
-
-NVIMRC_FILE    = HOME_DIR + '/.dotfiles/vimrc'
-NVIMRC_SYMLINK = HOME_DIR + '/.config/nvim/init.vim'
-
-confFiles = ['inputrc', 'tmux.conf', 'sqliterc', 'jshintrc', 'pylintrc',
-    'pythonrc', 'jsbeautifyrc', 'tern-config', 'ctags', 'agignore']
-
-# DONT include ~/.bashrc here. Remember that we use the default bashrc which
-# sources our bashrc
-# DONT include vim stuff here. Neovim and Vim have different setups nowadays so
-# it's not as easy as slapping on symlinks. The nvim setup is done in the
-# installation scripts and I don't use vim anymore
-others = {
-    HOME_DIR +'/.dotfiles/zshrc': HOME_DIR + '/.zshrc',
-}
-
-def unlink(link):
+def _unlink(link):
     msg = ""
     try:
         msg = "Removing:" + link
         os.unlink(link)
     except OSError as err:
-        msg = "{0}: {1}".format(err, link)
+        msg = str(err)
     finally:
         print(msg)
 
-def createSymlink(src, dst):
+
+def _createSymlink(src, dst):
     msg = ""
     try:
         msg = "[Success ] Creating symlink from: {} to: {}".format(src, dst)
         os.symlink(src, dst)
     except OSError as err:
-        msg = "{0}: {1}".format(err, dst)
+        msg = str(err)
     finally:
         print(msg)
+
+def main(clean):
+    HOME_DIR = os.path.expandvars('${HOME}')
+    CONF_DIR = HOME_DIR + '/.dotfiles/conf'
+
+    # TODO: Add checks if I move conf files dirs
+    conf_files = {
+        CONF_DIR + '/inputrc':              HOME_DIR + '/.inputrc',
+        CONF_DIR + '/tmux.conf':            HOME_DIR + '/.tmux.conf',
+        CONF_DIR + '/sqliterc':             HOME_DIR + '/.sqliterc',
+        CONF_DIR + '/jshintrc':             HOME_DIR + '/.jshintrc',
+        CONF_DIR + '/pylintrc':             HOME_DIR + '/.pylintrc',
+        CONF_DIR + '/pythonrc':             HOME_DIR + '/.pythonrc',
+        CONF_DIR + '/jsbeautifyrc':         HOME_DIR + '/.jsbeautifyrc',
+        CONF_DIR + '/tern-config':          HOME_DIR + '/.tern-config',
+        CONF_DIR + '/ctags':                HOME_DIR + '/.ctags',
+        CONF_DIR + '/agignore':             HOME_DIR + '/.agignore',
+        # File not in conf dir
+        HOME_DIR + '/.dotfiles/zshrc':      HOME_DIR + '/.zshrc',
+        HOME_DIR + '/.dotfiles/vimrc':      HOME_DIR + '/.config/nvim/init.vim',
+        HOME_DIR + '/.dotfiles/pre-commit': HOME_DIR + '/.dotfiles/.git/hooks/pre-commit'
+    }
+
+    for path, symlink in conf_files.items():
+        if clean:
+            _unlink(symlink)
+        else:
+            os.makedirs(os.path.dirname(path), exist_ok=True)
+            _createSymlink(path, symlink)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--clean', dest="clean", action="store_true",
-            help="Delete existing symlinks")
+                        help="Delete existing symlinks")
     args = parser.parse_args()
+    main(args.clean)
 
-    if args.clean:
-        for f in confFiles:
-            symlink = HOME_DIR + "/." + f
-            unlink(symlink)
-
-        for _, symlinkPath in others.items():
-            unlink(symlinkPath)
-
-        unlink(os.path.expandvars('${HOME}/.config/nvim/init.vim'))
-
-    else:
-        for f in confFiles:
-            realPath = CONF_DIR + "/" + f
-            symlinkPath = HOME_DIR + "/." + f
-            createSymlink(realPath, symlinkPath)
-
-        for realPath, symlinkPath in others.items():
-            createSymlink(realPath, symlinkPath)
-
-        os.makedirs(os.path.dirname(NVIMRC_SYMLINK), exist_ok=True)
-        createSymlink(NVIMRC_FILE, NVIMRC_SYMLINK)
---
+++ setup/setup_test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3.5
+import os
+import unittest
+from unittest.mock import patch
+import setup
+
+HOME_DIR = os.path.expandvars('${HOME}')
+CONF_DIR = HOME_DIR + '/.dotfiles/conf'
+
+conf_files = {
+    CONF_DIR + '/inputrc':              HOME_DIR + '/.inputrc',
+    CONF_DIR + '/tmux.conf':            HOME_DIR + '/.tmux.conf',
+    CONF_DIR + '/sqliterc':             HOME_DIR + '/.sqliterc',
+    CONF_DIR + '/jshintrc':             HOME_DIR + '/.jshintrc',
+    CONF_DIR + '/pylintrc':             HOME_DIR + '/.pylintrc',
+    CONF_DIR + '/pythonrc':             HOME_DIR + '/.pythonrc',
+    CONF_DIR + '/jsbeautifyrc':         HOME_DIR + '/.jsbeautifyrc',
+    CONF_DIR + '/tern-config':          HOME_DIR + '/.tern-config',
+    CONF_DIR + '/ctags':                HOME_DIR + '/.ctags',
+    CONF_DIR + '/agignore':             HOME_DIR + '/.agignore',
+    # File not in conf dir
+    HOME_DIR + '/.dotfiles/zshrc':      HOME_DIR + '/.zshrc',
+    HOME_DIR + '/.dotfiles/vimrc':      HOME_DIR + '/.config/nvim/init.vim',
+    HOME_DIR + '/.dotfiles/pre-commit': HOME_DIR + '/.dotfiles/.git/hooks/pre-commit'
+}
+
+class TestMain(unittest.TestCase):
+    @patch('os.makedirs')
+    @patch('os.symlink')
+    def test_main_link(self, os_symlink, os_makedirs):
+        setup.main(False)
+        for path, link in conf_files.items():
+            os_makedirs.assert_any_call(os.path.dirname(path), exist_ok=True)
+            os_symlink.assert_any_call(path, link)
+
+    @patch('os.unlink')
+    def test_main_unlink(self, os_unlink):
+        setup.main(True)
+        for _, link in conf_files.items():
+            os_unlink.assert_any_call(link)
+
+
+if __name__ == '__main__':
+    unittest.main()
