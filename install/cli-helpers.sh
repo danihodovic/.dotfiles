c444798b58aa437b16589d7c14b0e2ea1911a490
Dani Hodovic <dani.hodovic@gmail.com>
2016-02-22 20:29:50 +0100

    Uncomment unused programs. Add tldr and how2

--- install/cli-helpers.sh
+++ install/cli-helpers.sh
@@ -1,25 +1,38 @@
 #!/usr/bin/env bash
 
-read -p "Install bro?"                          INSTALL_BRO
-read -p "Install howdoi?"                       INSTALL_HOWDOI
-read -p "Install commandlinefu?"                INSTALL_CLF
+read -p "Install tldr? User defined summaries of man pages. Requires NodeJS" INSTALL_TLDR
+read -p "Install how2? Retrieves answers from stackoverflow. Requires NodeJS" INSTALL_HOW2
 
-case $INSTALL_BRO in
-    y|Y)
-        sudo apt-get install ruby ruby-dev -y
-        sudo gem install bropages
-        ;;
+case $INSTALL_TLDR in
+    y|Y) npm install tldr -g;;
 esac
 
-case $INSTALL_HOWDOI in
-    y|Y)
-        sudo apt-get install libxml2-dev libxslt1-dev -y
-        sudo pip install howdoi
-        ;;
+case $INSTALL_HOW2 in
+    y|Y) npm install how2 -g;;
 esac
 
-case $INSTALL_CLF in
-    y|Y)
-        sudo pip install clf
-        ;;
-esac
+# Unused below
+
+# read -p "Install bro?"                          INSTALL_BRO
+# read -p "Install howdoi?"                       INSTALL_HOWDOI
+# read -p "Install commandlinefu?"                INSTALL_CLF
+
+# case $INSTALL_BRO in
+    # y|Y)
+        # sudo apt-get install ruby ruby-dev -y
+        # sudo gem install bropages
+        # ;;
+# esac
+
+# case $INSTALL_HOWDOI in
+    # y|Y)
+        # sudo apt-get install libxml2-dev libxslt1-dev -y
+        # sudo pip install howdoi
+        # ;;
+# esac
+
+# case $INSTALL_CLF in
+    # y|Y)
+        # sudo pip install clf
+        # ;;
+# esac
