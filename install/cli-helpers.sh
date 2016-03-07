b260818dd1fa7e8906393782df1274624c8403f0
Dani Hodovic <dani.hodovic@gmail.com>
2016-03-07 01:15:40 +0100

    Removed unused cli tools

--- install/cli-helpers.sh
+++ install/cli-helpers.sh
@@ -11,28 +11,3 @@ case $INSTALL_HOW2 in
     y|Y) npm install how2 -g;;
 esac
 
-# Unused below
-
-# read -p "Install bro?"                          INSTALL_BRO
-# read -p "Install howdoi?"                       INSTALL_HOWDOI
-# read -p "Install commandlinefu?"                INSTALL_CLF
-
-# case $INSTALL_BRO in
-    # y|Y)
-        # sudo apt-get install ruby ruby-dev -y
-        # sudo gem install bropages
-        # ;;
-# esac
-
-# case $INSTALL_HOWDOI in
-    # y|Y)
-        # sudo apt-get install libxml2-dev libxslt1-dev -y
-        # sudo pip install howdoi
-        # ;;
-# esac
-
-# case $INSTALL_CLF in
-    # y|Y)
-        # sudo pip install clf
-        # ;;
-# esac
