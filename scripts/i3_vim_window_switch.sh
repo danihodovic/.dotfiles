09c30218c003e447729151bb91d4ae4c3537a35f
Dani Hodovic <dani.hodovic@gmail.com>
2016-04-07 18:49:47 +0200

    i3-vim switch: Fix stricter regex checking and logfile to /tmp

--- scripts/i3_vim_window_switch.sh
+++ scripts/i3_vim_window_switch.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
+logfile=/tmp/i3_vim_window_switch.log
 direction=$1
 active=$(xprop -id $(xdotool getwindowfocus) WM_NAME)
 
+is_vim_window_regex='vim"$'
+
 shopt -s nocasematch;
 
-if [[ $active =~ "vim" ]]; then
+echo "$active" >> "$logfile"
+
+if [[ $active =~ $is_vim_window_regex ]]; then
   key=''
   case $direction in
     up) key='k' ;;
@@ -15,7 +20,7 @@ if [[ $active =~ "vim" ]]; then
   cmd="g+w+l+${key}"
   xdotool getactivewindow key "$cmd"
 else
-  i3-msg focus $direction
+  i3-msg focus "$direction"
 fi
 
 shopt -u nocasematch;
