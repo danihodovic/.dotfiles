939f1292b29a632ffc7bc98082da7f944b98d71c
Dani Hodovic <dani.hodovic@gmail.com>
2016-08-12 00:42:48 +0200

    Fixed: Symlink vimrc for both vim and neovim
    When a dictionary was used the same key would only be used for one
    value. .vimrc would be missing for either vim or neovim. A list makes
    more sense

--- setupSymlinks.py
+++ setupSymlinks.py
@@ -15,29 +15,29 @@ HOME_DIR = os.path.expandvars('${HOME}')
 CONF_DIR = HOME_DIR + '/.dotfiles/conf'
 
 # TODO: Add checks if I move conf files dirs
-conf_files = {
-    CONF_DIR + '/profile':              HOME_DIR + '/.profile',
-    CONF_DIR + '/inputrc':              HOME_DIR + '/.inputrc',
-    CONF_DIR + '/tmux.conf':            HOME_DIR + '/.tmux.conf',
-    CONF_DIR + '/sqliterc':             HOME_DIR + '/.sqliterc',
-    CONF_DIR + '/jshintrc':             HOME_DIR + '/.jshintrc',
-    CONF_DIR + '/pylintrc':             HOME_DIR + '/.pylintrc',
-    CONF_DIR + '/pythonrc':             HOME_DIR + '/.pythonrc',
-    CONF_DIR + '/jsbeautifyrc':         HOME_DIR + '/.jsbeautifyrc',
-    CONF_DIR + '/tern-config':          HOME_DIR + '/.tern-config',
-    CONF_DIR + '/ctags':                HOME_DIR + '/.ctags',
-    CONF_DIR + '/agignore':             HOME_DIR + '/.agignore',
-    CONF_DIR + '/gitconfig':            HOME_DIR + '/.gitconfig',
-    CONF_DIR + '/global-gitignore':     HOME_DIR + '/.config/git/ignore',
-    CONF_DIR + '/i3-config':            HOME_DIR + '/.i3/config',
-    CONF_DIR + '/Xresources':           HOME_DIR + '/.Xresources',
-    CONF_DIR + '/vimperatorrc':         HOME_DIR + '/.vimperatorrc',
+conf_files = [
+    (CONF_DIR + '/profile',              HOME_DIR + '/.profile'),
+    (CONF_DIR + '/inputrc',              HOME_DIR + '/.inputrc'),
+    (CONF_DIR + '/tmux.conf',            HOME_DIR + '/.tmux.conf'),
+    (CONF_DIR + '/sqliterc',             HOME_DIR + '/.sqliterc'),
+    (CONF_DIR + '/jshintrc',             HOME_DIR + '/.jshintrc'),
+    (CONF_DIR + '/pylintrc',             HOME_DIR + '/.pylintrc'),
+    (CONF_DIR + '/pythonrc',             HOME_DIR + '/.pythonrc'),
+    (CONF_DIR + '/jsbeautifyrc',         HOME_DIR + '/.jsbeautifyrc'),
+    (CONF_DIR + '/tern-config',          HOME_DIR + '/.tern-config'),
+    (CONF_DIR + '/ctags',                HOME_DIR + '/.ctags'),
+    (CONF_DIR + '/agignore',             HOME_DIR + '/.agignore'),
+    (CONF_DIR + '/gitconfig',            HOME_DIR + '/.gitconfig'),
+    (CONF_DIR + '/global-gitignore',     HOME_DIR + '/.config/git/ignore'),
+    (CONF_DIR + '/i3-config',            HOME_DIR + '/.i3/config'),
+    (CONF_DIR + '/Xresources',           HOME_DIR + '/.Xresources'),
+    (CONF_DIR + '/vimperatorrc',         HOME_DIR + '/.vimperatorrc'),
     # File not in conf dir
-    HOME_DIR + '/.dotfiles/zshrc':      HOME_DIR + '/.zshrc',
-    HOME_DIR + '/.dotfiles/vimrc':      HOME_DIR + '/.config/nvim/init.vim',
-    HOME_DIR + '/.dotfiles/vimrc':      HOME_DIR + '/.vimrc',
-    HOME_DIR + '/.dotfiles/pre-commit': HOME_DIR + '/.dotfiles/.git/hooks/pre-commit'
-}
+    (HOME_DIR + '/.dotfiles/zshrc',      HOME_DIR + '/.zshrc'),
+    (HOME_DIR + '/.dotfiles/vimrc',      HOME_DIR + '/.config/nvim/init.vim'),
+    (HOME_DIR + '/.dotfiles/vimrc',      HOME_DIR + '/.vimrc'),
+    (HOME_DIR + '/.dotfiles/pre-commit', HOME_DIR + '/.dotfiles/.git/hooks/pre-commit')
+]
 
 def _removeLink(link, verbose=True):
     msg = ""
@@ -63,7 +63,7 @@ def _createSymlink(src, dst, verbose = True):
             print(msg)
 
 def main(verbose=True):
-    for path, symlink in conf_files.items():
+    for [path, symlink] in conf_files:
         _removeLink(symlink, verbose)
         if not os.path.isdir(os.path.dirname(symlink)):
             os.makedirs(os.path.dirname(symlink))
--- test/unit/setupSymlinks_test.py
+++ test/unit/setupSymlinks_test.py
@@ -18,29 +18,29 @@ if version < min_version:
 HOME_DIR = os.path.expandvars('${HOME}')
 CONF_DIR = HOME_DIR + '/.dotfiles/conf'
 
-conf_files = {
-    CONF_DIR + '/inputrc':              HOME_DIR + '/.inputrc',
-    CONF_DIR + '/tmux.conf':            HOME_DIR + '/.tmux.conf',
-    CONF_DIR + '/sqliterc':             HOME_DIR + '/.sqliterc',
-    CONF_DIR + '/jshintrc':             HOME_DIR + '/.jshintrc',
-    CONF_DIR + '/pylintrc':             HOME_DIR + '/.pylintrc',
-    CONF_DIR + '/pythonrc':             HOME_DIR + '/.pythonrc',
-    CONF_DIR + '/jsbeautifyrc':         HOME_DIR + '/.jsbeautifyrc',
-    CONF_DIR + '/tern-config':          HOME_DIR + '/.tern-config',
-    CONF_DIR + '/ctags':                HOME_DIR + '/.ctags',
-    CONF_DIR + '/agignore':             HOME_DIR + '/.agignore',
-    CONF_DIR + '/gitconfig':            HOME_DIR + '/.gitconfig',
-    CONF_DIR + '/global-gitignore':     HOME_DIR + '/.config/git/ignore',
-    CONF_DIR + '/Xresources':           HOME_DIR + '/.Xresources',
-    CONF_DIR + '/i3-config':            HOME_DIR + '/.i3/config',
-    CONF_DIR + '/vimperatorrc':         HOME_DIR + '/.vimperatorrc',
-    CONF_DIR + '/profile':              HOME_DIR + '/.profile',
+conf_files = [
+    (CONF_DIR + '/profile',              HOME_DIR + '/.profile'),
+    (CONF_DIR + '/inputrc',              HOME_DIR + '/.inputrc'),
+    (CONF_DIR + '/tmux.conf',            HOME_DIR + '/.tmux.conf'),
+    (CONF_DIR + '/sqliterc',             HOME_DIR + '/.sqliterc'),
+    (CONF_DIR + '/jshintrc',             HOME_DIR + '/.jshintrc'),
+    (CONF_DIR + '/pylintrc',             HOME_DIR + '/.pylintrc'),
+    (CONF_DIR + '/pythonrc',             HOME_DIR + '/.pythonrc'),
+    (CONF_DIR + '/jsbeautifyrc',         HOME_DIR + '/.jsbeautifyrc'),
+    (CONF_DIR + '/tern-config',          HOME_DIR + '/.tern-config'),
+    (CONF_DIR + '/ctags',                HOME_DIR + '/.ctags'),
+    (CONF_DIR + '/agignore',             HOME_DIR + '/.agignore'),
+    (CONF_DIR + '/gitconfig',            HOME_DIR + '/.gitconfig'),
+    (CONF_DIR + '/global-gitignore',     HOME_DIR + '/.config/git/ignore'),
+    (CONF_DIR + '/i3-config',            HOME_DIR + '/.i3/config'),
+    [CONF_DIR + '/Xresources',           HOME_DIR + '/.Xresources'],
+    (CONF_DIR + '/vimperatorrc',         HOME_DIR + '/.vimperatorrc'),
     # File not in conf dir
-    HOME_DIR + '/.dotfiles/zshrc':      HOME_DIR + '/.zshrc',
-    HOME_DIR + '/.dotfiles/vimrc':      HOME_DIR + '/.config/nvim/init.vim',
-    HOME_DIR + '/.dotfiles/vimrc':      HOME_DIR + '/.vimrc',
-    HOME_DIR + '/.dotfiles/pre-commit': HOME_DIR + '/.dotfiles/.git/hooks/pre-commit'
-}
+    (HOME_DIR + '/.dotfiles/zshrc',      HOME_DIR + '/.zshrc'),
+    (HOME_DIR + '/.dotfiles/vimrc',      HOME_DIR + '/.config/nvim/init.vim'),
+    (HOME_DIR + '/.dotfiles/vimrc',      HOME_DIR + '/.vimrc'),
+    (HOME_DIR + '/.dotfiles/pre-commit', HOME_DIR + '/.dotfiles/.git/hooks/pre-commit')
+]
 
 class TestMain(unittest.TestCase):
     def test_dict_equal(self):
@@ -48,7 +48,8 @@ class TestMain(unittest.TestCase):
         Ensures that the configuration files are the same as the ones defined in tests.
         Basically a double check
         '''
-        assert(set(conf_files.items()) == set(setupSymlinks.conf_files.items()))
+        sort_by_first = lambda tup: tup[0]
+        assert(conf_files.sort(key=sort_by_first) == setupSymlinks.conf_files.sort(key=sort_by_first))
 
     @patch('os.remove')
     @patch('os.symlink')
@@ -58,7 +59,7 @@ class TestMain(unittest.TestCase):
         '''
         setupSymlinks.main(verbose=False)
 
-        for path, link in conf_files.items():
+        for [path, link] in conf_files:
             os_remove.assert_any_call(link)
             os_symlink.assert_any_call(path, link)
 
@@ -66,6 +67,8 @@ class TestMain(unittest.TestCase):
         assert(os_symlink.call_count == len(conf_files))
 
 
+    # Note: We need to patch os.remove, os.symlink and os.path.isdir because otherwise
+    #  these commands will be executed on the machine
     @patch('os.remove')
     @patch('os.symlink')
     @patch('os.path.isdir')
@@ -77,7 +80,7 @@ class TestMain(unittest.TestCase):
         os_path_isdir.return_value = False
         setupSymlinks.main(verbose=False)
 
-        for _, link in conf_files.items():
+        for [_, link] in conf_files:
             os_makedirs.assert_any_call(os.path.dirname(link))
 
         assert(os_makedirs.call_count == len(conf_files))
