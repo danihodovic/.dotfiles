390dab82c8e390506f9f030ee3b2dcdb0595a706
Dani Hodovic <dani.hodovic@gmail.com>
2016-11-28 00:14:56 +0100

    Add: Usage to mps-youtube

--- scripts/mps-download.sh
+++ scripts/mps-download.sh
@@ -1,1 +1,11 @@
-drun -v $(pwd):/home/user/mps/ andrey01/mps-youtube daurl 'https://www.youtube.com/watch?v=I9ZsNCYGLCk'
+usage() {
+  echo "Invalid number of arguments."
+  echo "Usage: $0 <youtube-url>"
+  exit 1
+}
+
+if [ $# -ne 1 ]; then
+  usage
+fi
+
+docker run -v "$(pwd)":/home/user/mps/ andrey01/mps-youtube daurl "$1"
