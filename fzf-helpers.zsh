8bb8e5de959155b7c04552a910908bad80c2ba99
Dani Hodovic <dani.hodovic@gmail.com>
2017-01-15 22:38:57 +0100

    Change: Allow `fcommit` to select multiple commits

--- fzf-helpers.zsh
+++ fzf-helpers.zsh
@@ -78,7 +78,7 @@ fe() {
 fcommit() {
   local commits commit
   commits=$(git log --color=always --pretty=oneline --abbrev-commit --reverse) &&
-  commit=$(echo "$commits" | fzf --tac +s +m -e --ansi --reverse) &&
+  commit=$(echo "$commits" | fzf --tac +s -m -e --ansi --reverse) &&
   echo -n $(echo "$commit" | sed "s/ .*//")
 }
 
