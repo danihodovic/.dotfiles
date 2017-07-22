03f6d9fbf48acf0f462475ec29ecbefc14738f7a
Dani Hodovic <dani.hodovic@gmail.com>
2017-07-22 16:19:51 +0200

    Fix xfce4-terminalrc symlink

--- setup_symlinks.py
+++ setup_symlinks.py
@@ -33,7 +33,7 @@ conf_files = [
     (CONF_DIR + '/Xresources',           HOME_DIR + '/.Xresources'),
     (CONF_DIR + '/vimperatorrc',         HOME_DIR + '/.vimperatorrc'),
     (CONF_DIR + '/ginit.vim',            HOME_DIR + '/.config/nvim/ginit.vim'),
-    (CONF_DIR + '/xfce4-terminalrc/',    HOME_DIR + '/.config/xfce4/terminal/terminalrc'),
+    (CONF_DIR + '/xfce4-terminalrc',     HOME_DIR + '/.config/xfce4/terminal/terminalrc'),
     # File not in conf dir
     (HOME_DIR + '/.dotfiles/zlogin',     HOME_DIR + '/.zlogin'),
     (HOME_DIR + '/.dotfiles/zshrc',      HOME_DIR + '/.zshrc'),
